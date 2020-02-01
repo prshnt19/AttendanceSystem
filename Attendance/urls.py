05be8046b898918e0f9eab27b3e44e5ad9d9af2e
Abhishek <uchiha@pop-os.localdomain>
2020-02-02 00:14:24 +0530

    changed urls

--- Attendance/urls.py
+++ Attendance/urls.py
@@ -6,7 +6,7 @@ app_name = 'attendance'
 urlpatterns = [
     path("register/", registeradmin, name = "registeradmin"),
 
-    path("", registeradmin, name="registeradmin"),
+    # path("", registeradmin, name="registeradmin"),
     path("dashboard/", dashboard, name="dashboard"),
     # path("upload/", upload, name="upload"),
 ]
--- attendance_system/urls.py
+++ attendance_system/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('api/v1/create-auth/', attendance_views.register.as_view()),
     # django website views
     path("", include('Attendance.urls')),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('admin/', admin.site.urls),
