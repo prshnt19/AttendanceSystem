5d7ab25ec721dfbb822f581713489a601e5969db
prshnt19 <prashant.rawat216@gmail.com>
2020-02-01 19:08:42 +0530

    uuid token 5

--- Attendance/models.py
+++ Attendance/models.py
@@ -3,20 +3,22 @@ import uuid
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
 class Centers(models.Model):
     name = models.CharField(max_length=15, default=None)
     center_id = models.UUIDField( 
-         primary_key = True, 
-         default = uuid.uuid4, 
-         editable = False)
+         primary_key=True,
+         default=str(uuid.uuid4())[31:],
+         editable=False)
     voiceit_id = models.CharField(max_length=40, default=None)
+
 
 class UserProfile(models.Model):
     Center = models.ForeignKey(Centers, on_delete=models.CASCADE, )
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_Admin = models.NullBooleanField(default=None)
     voiceit_id = models.CharField(max_length=40)
+
 
 class AttendanceTable(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -25,6 +27,7 @@ class AttendanceTable(models.Model):
     voice_verified = models.NullBooleanField(default=None)
     face_verified = models.NullBooleanField(default=None)
 
+
 class Video(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    video = models.FileField(upload_to ='uploads/'+ str(user.name) +'/' )
+    video = models.FileField(upload_to='uploads/'+ str(user.name) +'/' )
--- Attendance/urls.py
+++ Attendance/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import registeradmin, dashboard
+from .views import registeradmin, dashboard, upload
 
 app_name = 'attendance'
 
 urlpatterns = [
-    path("", registeradmin, name = "registeradmin"),
+    path("", registeradmin, name="registeradmin"),
     path("dashboard/", dashboard, name="dashboard"),
+    # path("upload/", upload, name="upload"),
 ]
