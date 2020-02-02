cb3442644550e549d52f4798ea342df3e6329d05
prshnt19 <prashant.rawat216@gmail.com>
2020-02-02 11:55:52 +0530

    dashboard

--- Attendance/admin.py
+++ Attendance/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from .models import UserProfile, Centers, Video, AttendanceTable
 
-# Register your models here.
-admin.site.register(UserProfile)
-admin.site.register(Centers)
-admin.site.register(Video)
-admin.site.register(AttendanceTable)
+
+class UserProfileEntry(admin.ModelAdmin):
+    list_display = ("user", "center", "voiceit_id", "contact_number")
+
+
+class AttendanceTableEntry(admin.ModelAdmin):
+    list_display = ("user", "center", "date", "location_verified", "voice_face_verified", "present")
+
+
+class CenterEntry(admin.ModelAdmin):
+    list_display = ("name", "center_id", "voiceit_id", "latitude", "longitude")
+
+
+class VideoEntry(admin.ModelAdmin):
+    list_display = ("user", "video")
+
+
+admin.site.register(AttendanceTable, AttendanceTableEntry)
+admin.site.register(Centers, CenterEntry)
+admin.site.register(Video, VideoEntry)
+admin.site.register(UserProfile, UserProfileEntry)
